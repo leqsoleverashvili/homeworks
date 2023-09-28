272aac49bcb426edf55aae6794a6640bfa47f30d
leqsoleverashvili <146107785+leqsoleverashvili@users.noreply.github.com>
2023-09-28 17:33:29 +0400

    Add files via upload

---
+++ day2hm.py
@@ -0,0 +1,12 @@
+dad_age = 49
+my_age = 14
+mom_age = 48
+grandma_age =83
+sister_age = 20
+age = (grandma_age, mom_age, my_age, dad_age, sister_age)
+
+ramdenjer_metia = (dad_age / my_age)
+
+print(ramdenjer_metia)
+
+print(age)
