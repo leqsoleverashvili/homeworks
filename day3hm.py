c45ce5ee9f453fa9c8bad30f1a9bcf9f3ea0353b
leqsoleverashvili <146107785+leqsoleverashvili@users.noreply.github.com>
2023-10-04 22:57:48 +0400

    Add files via upload

---
+++ day3hm.py
@@ -0,0 +1,11 @@
+dad_age = 49
+my_age = 14
+mom_age = 48
+grandma_age =83
+sister_age = 20
+new_sister_age = sister_age + 20
+new_grandma_age = grandma_age + 20
+new_mom_age = mom_age + 20
+my_new_age = my_age + 20
+new_dad_age = dad_age + 20
+print(new_mom_age, new_dad_age, new_grandma_age, new_sister_age, my_new_age)
