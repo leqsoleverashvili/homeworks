f9e999cf8e34ef6a748d7b6fc359d1a1f3021d03
leqsoleverashvili <146107785+leqsoleverashvili@users.noreply.github.com>
2023-10-08 14:03:25 +0400

    Add files via upload

---
+++ day4hm.py
@@ -0,0 +1,7 @@
+salary = input("what is your salary")
+if salary > 10000:
+    print("goashi swavlobdi?")
+if salary <1000 >10000:
+    print("you MID")
+if salary <1000:
+    print("shemosuliyavi goashi matricelo")
