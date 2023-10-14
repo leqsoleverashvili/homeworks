94864f73545b071ae193a432035d93aeac0aa18a
leqsoleverashvili <146107785+leqsoleverashvili@users.noreply.github.com>
2023-10-14 23:16:04 +0400

    Update day6hm.py

---
+++ day6hm.py
@@ -0,0 +1,9 @@
+zvelibiwebi = 10
+jemalikoebi = 3
+ticket_price = 25
+sul = 0
+while zvelibiwebi > 0:
+    sul += ticket_price
+    zvelibiwebi -= 1
+sul += (jemalikoebi * 0)
+print("sul fasi: $", sul)
