37c00416667d9beb5022ff305a44bb903854d030
leqsoleverashvili <146107785+leqsoleverashvili@users.noreply.github.com>
2023-10-28 23:10:09 +0400

    Add files via upload

---
+++ day8hm1.py
@@ -0,0 +1,6 @@
+family = ["gvanca", "soso", "leqso"]
+age = [48, 50, 14]
+full_sentence = "my mom's age is: {}, my dad's age is: {}, my age is: {}".format(age[0], age[1], age[2])
+print(full_sentence)
+after_ten_years = "my moms age is:" , age[0] + 10, "my dads age is:" , age[1] + 10, "my age is:" , age[2] + 10
+print(after_ten_years)
