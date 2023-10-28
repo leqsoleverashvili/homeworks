bfe49aa902628ea9529b9edbb5af68317fdfa103
leqsoleverashvili <146107785+leqsoleverashvili@users.noreply.github.com>
2023-10-28 22:46:15 +0400

    Add files via upload

---
+++ calculator_basic.py
@@ -0,0 +1,13 @@
+num1 = float(input("priveli ricxvi "))
+num2 = float(input("merore ricxvi "))
+
+operacia = input("arichiet operacia: +, -, *, /")
+if operacia == "+":
+ print(num1 + num2)
+if operacia == "-":
+ print(num1 - num2)
+if operacia == "*":
+ print(num1 * num2)
+if operacia == "/":
+ print(num1 / num2)
+
